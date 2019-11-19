411cf46be2975c2d5a0b86f6222036a7ffc20ad2
myazejea <myazejea@uncc.edu>
2019-11-19 15:11:01 -0500

    update

---
+++ models/fan-connection.js
@@ -0,0 +1,29 @@
+var Connect = require('.connect');
+
+class FanConnection{
+  constructor(connect, rspv){
+    if(connect instanceof Connect){
+      this._connect = connect;
+    }else{
+      throw new Error('connect should be object');
+    }
+    this._rspv = rspv;
+  }
+  get connect(){
+    return this._connect;
+  }
+  set connect(value){
+    if(value instanceof Connect){
+      this._connect = value;
+    }else{
+      throw new Error('connect should be object');
+    }
+  }
+  get rspv(){
+    return this._rspv;
+  }
+  set rspv(value){
+    this._rspv = value;
+  }
+}
+module.exports = FanConnection;
--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ class User{
     this._firstName = firstName;
     this._lastName = lastName;
     this._email = email;
+    this._password = password;
   }
   get userId(){
     return this._userId;
@@ -17,17 +18,23 @@ class User{
   get email(){
     return this._email;
   }
+  get password(){
+    return this._password;
+  }
+  set password(value){
+    this._password = value;
+  }
   set userId(value){
    this._userId = value;
  }
    set firstName(value){
     this._firstName = value;
-}
-set lastName(value){
-  this._lastName = value;
-}
-set email(value){
- this._email = value;
-}
+  }
+  set lastName(value){
+    this._lastName = value;
+  }
+  set email(value){
+   this._email = value;
+  }
 }
 module.exports = User;
